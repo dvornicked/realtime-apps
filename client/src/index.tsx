b536934f9ea8a50e78f241170b5ee897d21f479e
Dvornick <mail@dvornick.ru>
2022-07-30 12:24:55 +0300

    Initial commit

---
+++ client/src/index.tsx
@@ -0,0 +1,5 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<ChakraProvider></ChakraProvider>)
